8b08f5df06c112937588ffc917eade0999f922d5
Paweł Galewicz <pawel.galewicz@xyb.co>
2024-09-02 12:54:45 +0200

    restructure project to have separate maven modules for consumer, producer and common dtos

---
+++ common/src/test/java/com/pg/replication/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg.replication.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- src/main/java/com/pg/replication/consumer/v2/ReplicationType.java
+++
@@ -1,5 +0,0 @@
-package com.pg.replication.consumer.v2;
-
-public enum ReplicationType {
-    MASTER, REPLICA
-}
